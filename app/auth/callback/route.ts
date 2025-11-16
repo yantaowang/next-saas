2428bbe0611135ac2e3caa5a32f7f9d98a15891f
智慧源点 <l_o_e@163.com>
2025-11-16 15:26:10 +0800

    origin

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,19 +1,19 @@
-import { createClient } from "@/lib/supabase/server";
-import { NextResponse } from "next/server";
+// import { createClient } from "@/lib/supabase/server";
+// import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const requestUrl = new URL(request.url);
-  const code = requestUrl.searchParams.get("code");
-  const next = requestUrl.searchParams.get("next") ?? "/";
+// export async function GET(request: Request) {
+//   const requestUrl = new URL(request.url);
+//   const code = requestUrl.searchParams.get("code");
+//   const next = requestUrl.searchParams.get("next") ?? "/";
 
-  console.log("requestUrl", requestUrl);
+//   console.log("requestUrl", requestUrl);
 
-  if (code) {
-    const supabase = await createClient();
-    await supabase.auth.exchangeCodeForSession(code);
-  }
+//   if (code) {
+//     const supabase = await createClient();
+//     await supabase.auth.exchangeCodeForSession(code);
+//   }
 
-  // 重定向到指定页面或首页
-  return NextResponse.redirect(new URL(next, requestUrl.origin));
-}
+//   // 重定向到指定页面或首页
+//   return NextResponse.redirect(new URL(next, requestUrl.origin));
+// }
 
